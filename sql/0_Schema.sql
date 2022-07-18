70993065df33782b1212ea423880423b77d1ea04
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-07-19 03:02:01 +0900

    Fix sql syntax

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -39,34 +39,34 @@ CREATE TABLE `isu_association_config` (
 ALTER TABLE `isu_condition` ADD INDEX idx_jia_isu_uuid_timestamp(`jia_isu_uuid`, `timestamp`);
 
 ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) GENERATED ALWAYS AS (CASE 
- WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=true' THEN 0
- WHEN condition = 'is_dirty=true,is_overweight=false,is_broken=false' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=true' THEN 0
+ WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=false' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
 
 ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) GENERATED ALWAYS AS (CASE 
- WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 1
- WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=true' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=false,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 0
- WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 1
+ WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 0
+ WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
 
 ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) GENERATED ALWAYS AS (CASE 
- WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 1
- WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=true' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=false,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 1
- WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 0
- WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 1
+ WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 0
+ WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
