0baa9dd62d4f616ec40c5ae16bd921c4c0555521
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-07-19 02:55:58 +0900

    Map generated column

--- go/main.go
+++ go/main.go
@@ -81,13 +81,16 @@ type GetIsuListResponse struct {
 }
 
 type IsuCondition struct {
-	ID         int       `db:"id"`
-	JIAIsuUUID string    `db:"jia_isu_uuid"`
-	Timestamp  time.Time `db:"timestamp"`
-	IsSitting  bool      `db:"is_sitting"`
-	Condition  string    `db:"condition"`
-	Message    string    `db:"message"`
-	CreatedAt  time.Time `db:"created_at"`
+	ID           int       `db:"id"`
+	JIAIsuUUID   string    `db:"jia_isu_uuid"`
+	Timestamp    time.Time `db:"timestamp"`
+	IsSitting    bool      `db:"is_sitting"`
+	Condition    string    `db:"condition"`
+	Message      string    `db:"message"`
+	CreatedAt    time.Time `db:"created_at"`
+	isDirty      bool      `db:"is_dirty"`
+	isOverweight bool      `db:"is_overweight"`
+	isBroken     bool      `db:"is_broken"`
 }
 
 type MySQLConnectionEnv struct {
