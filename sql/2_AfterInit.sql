c38758dde170d3eb8da5d202d1a0c4e790cf5742
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-07-19 03:27:56 +0900

    Add not null constraint

--- sql/2_AfterInit.sql
+++ sql/2_AfterInit.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
@@ -9,7 +9,7 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) GENERATED ALWAYS AS
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 1
@@ -20,7 +20,7 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) GENERATED ALWA
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 1
  WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
