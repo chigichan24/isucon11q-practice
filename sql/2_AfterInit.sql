2709c81417027d3a3e693c6d40e227a8442582ac
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-07-19 03:35:39 +0900

    Fix

--- sql/2_AfterInit.sql
+++ sql/2_AfterInit.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) GENERATED ALWAYS AS (CASE 
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
@@ -7,9 +7,9 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) NOT NULL GENERATED 
  WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 1
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 1
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
- END) VIRTUAL;
+ END) STORED;
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) GENERATED ALWAYS AS (CASE 
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 1
@@ -18,9 +18,9 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) NOT NULL GENER
  WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 0
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 1
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
- END) VIRTUAL;
+ END) STORED;
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) GENERATED ALWAYS AS (CASE 
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 1
  WHEN `condition` = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
@@ -29,4 +29,4 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) NOT NULL GENERATED
  WHEN `condition` = 'is_dirty=true,is_overweight=false,is_broken=true' THEN 1
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=false' THEN 0
  WHEN `condition` = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
- END) VIRTUAL;
+ END) STORED;
