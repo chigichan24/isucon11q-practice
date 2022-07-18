71f99082e64467eda472c85d1449965bb03ab8cd
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-07-19 02:59:05 +0900

    Remove not null

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE `isu_association_config` (
 
 ALTER TABLE `isu_condition` ADD INDEX idx_jia_isu_uuid_timestamp(`jia_isu_uuid`, `timestamp`);
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) GENERATED ALWAYS AS (CASE 
  WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
  WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
@@ -49,7 +49,7 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_dirty` TINYINT(1) NOT NULL GENERATED 
  WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) GENERATED ALWAYS AS (CASE 
  WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 0
  WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 1
@@ -60,7 +60,7 @@ ALTER TABLE `isu_condition` ADD COLUMN `is_overweight` TINYINT(1) NOT NULL GENER
  WHEN condition = 'is_dirty=true,is_overweight=true,is_broken=true' THEN 1
  END) VIRTUAL;
 
-ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) NOT NULL GENERATED ALWAYS AS (CASE 
+ALTER TABLE `isu_condition` ADD COLUMN `is_broken` TINYINT(1) GENERATED ALWAYS AS (CASE 
  WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=false' THEN 0
  WHEN condition = 'is_dirty=false,is_overweight=false,is_broken=true' THEN 1
  WHEN condition = 'is_dirty=false,is_overweight=true,is_broken=false' THEN 0
